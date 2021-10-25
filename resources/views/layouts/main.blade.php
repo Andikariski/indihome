d29078fc7394ba41094fafef3323f8f5adbe9a9b
Andikariski <andikariski207@gmail.com>
2021-10-26 00:15:42 +0700

    new

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,18 @@
+@include('layouts.header')
+  <body>
+    <div class="flex h-screen bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen }">
+      <!-- Include sidebar -->
+        @include('layouts.sidebar')
+      
+        <!-- Include navbar -->
+        @include('layouts.navbar')
+      
+        <!-- Main Content -->
+        <main class="h-full overflow-y-auto">
+          <section class="content">
+            @yield('container')
+        </section>
+        </main>
+    </div>
+  </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
+// use App\Http\Controllers\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // return view('login');
+    return view('auth.login');
+});
+
+Auth::routes(['register' => false]);
+
+Route::get('logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
+Route::get('/forecastError', [App\Http\Controllers\Admin\ForecastErrorController::class, 'index'])->name('forecast');
+Route::get('/trendPlot', [App\Http\Controllers\Admin\TrendPlotController::class, 'index'])->name('trendPlot');
+Route::get('/trendAnalysis', [App\Http\Controllers\Admin\TrendAnalysisController::class, 'index'])->name('trendAnalysis');
